8f04c7a1c09991bab6e659ab9eed91b6926e86a1
Sascha Becker <s.becker@wertarbyte.com>
2021-01-10 17:20:00 +0100

    Disable debug by default

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -5,7 +5,7 @@ import { Configuration } from './types';
 const config: Configuration = {
   amountPerInvest: 50.0,
   blacklist: ['DASH', 'GRIN', 'XMR', 'ZEC'],
-  debug: true,
+  debug: false,
   HODL: ['BTC', 'ETH'],
   refreshTimeout: 60000 * 60 * 0.05, // 3 min
   tickInterval: CandleInterval.DAY_1,
